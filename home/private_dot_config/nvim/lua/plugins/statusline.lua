168476b273ec8ad44d1129183534b4cd83843173
Gheorghita Hurmuz <hurmuz.gheorghita@pm.me>
2023-11-27 22:02:44 +0200

    feat: edit statusline

--- home/private_dot_config/nvim/lua/plugins/statusline.lua
+++ home/private_dot_config/nvim/lua/plugins/statusline.lua
@@ -7,7 +7,29 @@ return {
         component_separators = { left = "│", right = "│" },
         section_separators = { left = "", right = "" },
         globalstatus = true,
-        disabled_filetypes = { "NvimTree" },
+        -- disabled_filetypes = { "NvimTree" },
+      },
+      sections = {
+        lualine_b = {
+          {
+            "branch",
+          },
+          {
+            "diff",
+          },
+          {
+            "diagnostics",
+            symbols = { error = " ", warn = " ", info = " ", hint = " " },
+          },
+        },
+        lualine_c = {
+          {
+            "filename",
+          },
+        },
+      },
+      extensions = {
+        "nvim-tree",
       },
     },
   },
--- home/private_dot_config/nvim/lua/plugins/treesitter.lua
+++ home/private_dot_config/nvim/lua/plugins/treesitter.lua
@@ -1,51 +1,51 @@
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    dependencies = {
-      "nvim-treesitter/nvim-treesitter-textobjects",
-      "windwp/nvim-ts-autotag",
-    },
-    config = function()
-      local treesitter = require("nvim-treesitter.configs")
+	{
+		"nvim-treesitter/nvim-treesitter",
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter-textobjects",
+			"windwp/nvim-ts-autotag",
+		},
+		config = function()
+			local treesitter = require("nvim-treesitter.configs")
 
-      treesitter.setup({ -- enable syntax highlighting
-        highlight = {
-          enable = true,
-          additional_vim_regex_highlighting = false,
-        },
-        indent = { enable = true },
-        autotag = {
-          enable = true,
-        },
-        ensure_installed = {
-          "bash",
-          "json",
-          "javascript",
-          "typescript",
-          "yaml",
-          "html",
-          "css",
-          "markdown",
-          "markdown_inline",
-          "bash",
-          "lua",
-          "vim",
-          "gitignore",
-        },
-        incremental_selection = {
-          enable = true,
-          keymaps = {
-            init_selection = "<C-space>",
-            node_incremental = "<C-space>",
-            scope_incremental = false,
-            node_decremental = "<bs>",
-          },
-        },
-        context_commentstring = {
-          enable = true,
-          enable_autocmd = false,
-        },
-      })
-    end,
-  },
+			treesitter.setup({
+				highlight = {
+					enable = true,
+					additional_vim_regex_highlighting = false,
+				},
+				indent = { enable = true },
+				autotag = {
+					enable = true,
+				},
+				ensure_installed = {
+					"bash",
+					"json",
+					"javascript",
+					"typescript",
+					"yaml",
+					"html",
+					"css",
+					"markdown",
+					"markdown_inline",
+					"bash",
+					"lua",
+					"vim",
+					"gitignore",
+				},
+				incremental_selection = {
+					enable = true,
+					keymaps = {
+						init_selection = "<C-space>",
+						node_incremental = "<C-space>",
+						scope_incremental = false,
+						node_decremental = "<bs>",
+					},
+				},
+				context_commentstring = {
+					enable = true,
+					enable_autocmd = false,
+				},
+			})
+		end,
+	},
 }
