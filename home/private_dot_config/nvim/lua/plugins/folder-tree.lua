01e2acf678d8d877cc864fe72ef066dc7e33e84b
Gheorghita Hurmuz <hurmuz.gheorghita@pm.me>
2023-12-09 17:25:22 +0200

    add auto close config for nvim tree

--- home/private_dot_config/nvim/lua/plugins/folder-tree.lua
+++ home/private_dot_config/nvim/lua/plugins/folder-tree.lua
@@ -13,8 +13,28 @@ return {
 
       require("nvim-tree").setup({
         view = {
+          cursorline = true,
           side = "right",
           width = 30,
+        },
+        renderer = {
+          indent_width = 2,
+          indent_markers = {
+            enable = false,
+            inline_arrows = true,
+            icons = {
+              corner = "└",
+              edge = "│",
+              item = "│",
+              bottom = "─",
+              none = " ",
+            },
+          },
+        },
+        update_focused_file = {
+          enable = true,
+          update_root = true,
+          ignore_list = {},
         },
       })
 
