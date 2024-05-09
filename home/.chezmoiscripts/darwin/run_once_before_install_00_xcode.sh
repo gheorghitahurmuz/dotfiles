fa56a6322ea42e67ae0e9ecaf22e8ddeb651725d
Gheorghita Hurmuz <hurmuz.gheorghita@pm.me>
2024-05-09 17:11:13 +0300

    Fix script for install xcode

--- home/.chezmoiscripts/darwin/run_once_before_install_00_xcode.sh
+++ home/.chezmoiscripts/darwin/run_once_before_install_00_xcode.sh
@@ -1,10 +1,16 @@
 #!/usr/bin/env bash
 
-# set -eufo pipefail
-
-is_installed = $(xcode-select -p >/dev/null 2>&1; echo $?)
-
-if ! $is_installed; then
-    install = $(xcode-select --install)
-    read installed
+xcode-select --install >/dev/null 2>&1
+if [ 0 == $? ]; then
+  sleep 1
+  osascript <<EOD
+tell application "System Events"
+  tell process "Install Command Line Developer Tools"
+    keystroke return
+    click button "Agree" of window "License Agreement"
+  end tell
+end tell
+EOD
+else
+  echo "Command Line Developer Tools are already installed!"
 fi
