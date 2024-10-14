3da9f5308d578eb6fb3b9c273720a4c1913a0e07
Gheorghita Hurmuz <hurmuz.gheorghita@pm.me>
2024-10-14 17:54:29 +0300

    debug brew

--- home/.chezmoiscripts/darwin/run_onchange_before_00_install_homebrew.sh
+++ home/.chezmoiscripts/darwin/run_onchange_before_00_install_homebrew.sh
@@ -6,4 +6,7 @@ if ! command -v brew >/dev/null 2>&1; then
   echo "==> 📜 Install Homebrew"
 
   (/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)")
+  eval "$(homebrew/bin/brew shellenv)"
+else
+  echo "Homebrew already installed"
 fi
