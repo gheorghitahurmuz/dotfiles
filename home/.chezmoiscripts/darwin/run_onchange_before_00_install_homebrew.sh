6b56566397f4c66d6945149faa186d1edfbf5368
Gheorghita Hurmuz <hurmuz.gheorghita@pm.me>
2024-10-15 15:55:00 +0300

    use brew shellenv for arm64

--- home/.chezmoiscripts/darwin/run_onchange_before_00_install_homebrew.sh
+++ home/.chezmoiscripts/darwin/run_onchange_before_00_install_homebrew.sh
@@ -2,19 +2,10 @@
 
 set -eufo pipefail
 
-brew_path() {
-    if [[ $(uname -m) == "arm64" ]]; then
-        echo "/opt/homebrew/bin/brew"
-    else
-        echo "/usr/local/bin/brew"
-    fi
-}
+if ! command -v brew >/dev/null 2>&1; then
+  echo "==> 📜 Install Homebrew"
 
-if [[ $(command -v brew) || -x $(brew_path) ]]; then
-    echo "Homebrew already installed"
+  (/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)")
 else
-    echo "==> 📜 Install Homebrew"
-
-    (/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)")
-    eval "$("$(brew_path)" shellenv)"
+  echo "==> 📜 Homebrew already installed"
 fi
